571c81e3eff21072caef7dd81ab4aa4aceaa3c60
Dan Smith <dan.smith@theappbusiness.com>
2020-09-15 14:16:29 +0100

    Adds battle component

--- app/index.js
+++ app/index.js
@@ -2,12 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import './index.css'
 import Popular from './components/popular'
-
+import Battle from "./components/battle"
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <Popular />
+        {/* <Popular /> */}
+        <Battle />
       </div>
     )
   }
