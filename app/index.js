3ce735c47872286b5ec2b7480dfe8483009d1792
Dan Smith <dan.smith@theappbusiness.com>
2020-09-22 14:29:25 +0100

    checks Popular path

--- app/index.js
+++ app/index.js
@@ -7,8 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        {/* <Popular /> */}
-        <Battle />
+        <Popular />
       </div>
     )
   }
