99423eb23117cad81f58a525df7f1f6260356998
Dan Smith <dan.smith@theappbusiness.com>
2020-09-23 15:17:16 +0100

    Adds react-router and sets up basic routing for popular and battle

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import Popular from "./components/popular"
 import Battle from "./components/battle"
 import { ThemeProvider } from "./contexts/theme"
 import Nav from "./components/nav"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 class App extends React.Component {
   constructor(props) {
@@ -21,15 +22,17 @@ class App extends React.Component {
   }
   render() {
     return (
-      <ThemeProvider value={this.state}>
-        <div className={this.state.theme}>
-          <div className="container">
-            <Nav />
-
-            <Popular />
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+              <Route path="/battle" component={Battle} />
+              <Route exact path="/" component={Popular} />
+            </div>
           </div>
-        </div>
-      </ThemeProvider>
+        </ThemeProvider>
+      </Router>
     )
   }
 }
