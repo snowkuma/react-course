ad1f1b4f12dcde93578cb3e3f9e121af68201b4b
Dan Smith <dan.smith@theappbusiness.com>
2020-09-23 12:43:38 +0100

    Adds a theme context to the application

--- app/index.js
+++ app/index.js
@@ -3,12 +3,28 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import Popular from "./components/popular"
 import Battle from "./components/battle"
+import { ThemeProvider } from "./contexts/theme"
+
 class App extends React.Component {
+  constructor() {
+    super(props)
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === "light" ? "dark" : "light",
+        }))
+      },
+    }
+  }
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Popular />
+        </div>
+      </ThemeProvider>
     )
   }
 }
